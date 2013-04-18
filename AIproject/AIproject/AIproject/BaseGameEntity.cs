b023623f74f583dea8cdc6a084361e0d0c2e7b96
A.J. Geitenbeek <arjan_geitenbeek@hotmail.com>
2013-04-18 15:30:10 +0200

    added old feature-structure into project

---
+++ AIproject/AIproject/AIproject/BaseGameEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace AIproject
+{
+    abstract class BaseGameEntity
+    {
+        public int ID { get; set; }
+        public Vector2 Pos { get; set; }
+        public float Scale { get; set; }
+        public float Bradius { get; set; }
+
+        public abstract void Update(double time_elapsed);
+        public abstract void Render();
+    }
+}
---
+++ AIproject/AIproject/AIproject/MovingEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace AIproject
+{
+    abstract class MovingEntity : BaseGameEntity
+    {
+        public abstract override void Update(double time_elapsed);
+        public abstract override void Render();
+
+        public Vector2 Velocity { get; set; }
+        public Vector2 Heading { get; set; }
+        public float Mass { get; set; }
+        public float MaxSpeed { get; set; }
+        public float MaxForce { get; set; }
+        public float MaxTurnRate { get; set; }
+
+    }
+}
