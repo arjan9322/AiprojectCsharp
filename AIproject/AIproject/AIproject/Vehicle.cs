1da758222ad96ded80f3281b013c41a851cfd382
A.J. Geitenbeek <arjan_geitenbeek@hotmail.com>
2013-04-18 15:50:05 +0200

    my stuff

--- AIproject/AIproject/AIproject/Vehicle.cs
+++ AIproject/AIproject/AIproject/Vehicle.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using Microsoft.Xna.Framework;
 namespace AIproject
 {
     class Vehicle : MovingEntity
     {
-        
+        public Vehicle(Vector2 startingPos)
+        {
+            Pos = startingPos;
+            Velocity = new Vector2();
+        }
+        public override void Render()
+        {
+            throw new NotImplementedException();
+        }
+        public override void Update(double time_elapsed)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
